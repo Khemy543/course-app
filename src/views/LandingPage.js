f7564e6b24be851e7ce852232b9068a07968a9c6
GideonAssafuah <gideon@walulel.com>
2021-03-24 22:51:33 +0000

    first commit

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -194,7 +194,7 @@ function LandingPage(){
             </Nav>
           </Collapse>
         </Container>
-      </Navbar>
+      </Navbar> 
       
       <div 
             onClick={()=>handleAppClick()}>
